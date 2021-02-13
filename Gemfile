1701f390b7eb349f97fb43659e2740c3db1672cc
Murat Toygar <toygar-murat@hotmail.com>
2021-02-13 20:45:44 +0300

    add test to ensure seesaw is called no more than 3 times

--- Gemfile
+++ Gemfile
@@ -8,4 +8,6 @@ group :development, :test do
   gem 'rubocop', '~> 1.3', require: false
   # https://github.com/rails/rails/tree/main/activesupport
   gem 'activesupport'
+  # A simple opinionated mocking framework. All the features of rspec-mocks and more in half the code. (https://github.com/ryanong/spy)
+  gem 'spy'
 end
--- test/nine_9_riddle_test.rb
+++ test/nine_9_riddle_test.rb
@@ -1,25 +1,42 @@
 # frozen_string_literal: true
 
-require 'active_support'
 require 'minitest/autorun'
+require 'spy/integration'
+require 'active_support'
 require 'nine_9_riddle'
 
 class Nine9RiddleTest < ActiveSupport::TestCase
-  (0..11).each do |index|
-    test "#{index}th person is heavier" do
-      assert_equal index, Nine9Riddle.new(create_weight_array(index, 0.1)).solve
+  (0..11).each do |outlier_index|
+    test "#{outlier_index}th person is heavier" do
+      assert_equal outlier_index, Nine9Riddle.new(create_weight_array(outlier_index, 0.1)).solve
     end
 
-    test "#{index}th person is lighter" do
-      assert_equal index, Nine9Riddle.new(create_weight_array(index, -0.1)).solve
+    test "#{outlier_index}th person is lighter" do
+      assert_equal outlier_index, Nine9Riddle.new(create_weight_array(outlier_index, -0.1)).solve
+    end
+
+    test "uses seesaw 3 times when #{outlier_index}th person is heavier" do
+      seesaw_spy = Spy.on(Seesaw, :new).and_call_through
+
+      Nine9Riddle.new(create_weight_array(outlier_index, 0.1)).solve
+
+      assert_equal 3, seesaw_spy.calls.count
+    end
+
+    test "uses seesaw 3 times when #{outlier_index}th person is lighter" do
+      seesaw_spy = Spy.on(Seesaw, :new).and_call_through
+
+      Nine9Riddle.new(create_weight_array(outlier_index, -0.1)).solve
+
+      assert_equal 3, seesaw_spy.calls.count
     end
   end
 
   private
 
-  def create_weight_array(index, weight_difference)
+  def create_weight_array(outlier_index, weight_difference)
     weight_array = Array.new(12, 1)
-    weight_array[index] = weight_array.at(index) + weight_difference
+    weight_array[outlier_index] = weight_array.at(outlier_index) + weight_difference
 
     weight_array
   end
